b9f7d8fc1f24985b5763bb51d50b3006f8169cf7
Charlie Stewert <cforster@dalton.org>
2022-11-02 09:41:22 -0400

    end of class 2

--- webe-app/src/App.js
+++ webe-app/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const user ={
     name: 'Hedy Lamarr',
     imageUrl: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTgwNDM3MzQ0NzgyOTE5NTMy/bombshell_hedylamarr1.jpg',
@@ -20,10 +22,40 @@ function Profile() {
     )
 }
 
-function MyButton() {
+const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+  ];
+  
+function ShoppingList() {
+    const listItems = products.map(product =>
+      <li
+        key={product.id}
+        style={{
+          color: product.isFruit ? 'magenta' : 'darkgreen'
+        }}
+      >
+        {product.title}
+      </li>
+    );
+  
     return (
-        <button>
-            I'm a button
+      <ul>{listItems}</ul>
+    );
+  }
+
+function MyButton() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        alert('You clicked me!');
+        setCount(count+1);
+    }
+    
+    return (
+        <button onClick={handleClick}>
+            I'm a button that's been clicked {count} times
         </button>
     )
 }
@@ -33,7 +65,9 @@ export default function MyApp() {
         <div>
             <h1>Welcome to my app</h1>
             <MyButton />
+            <MyButton />
             <Profile />
+            <ShoppingList />
         </div>
     );
 }
